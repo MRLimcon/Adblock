385dc6d5614e8e08cc46b11f56c01dc46863e764
Matheus <78980755+MRLimcon@users.noreply.github.com>
2021-03-04 16:29:57 -0300

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,12 +1,8 @@
 var enabled = true;
 chrome.webRequest.onBeforeRequest.addListener(
 	function(details) {
-		if(enabled) {
-			console.log("blocking:", details.url);
-		}
-		return {cancel: enabled };
+	  return {cancel: details.url != -1};
 	},
 	{urls: blocked_domains},
-	//{urls: ["<all_urls>"]}, /* replace with list of blacklisted urls */
 	["blocking"]
 );
