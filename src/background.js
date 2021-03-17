0ad3d27c5dfd15e4f58be14b259c979acc6a28ef
pedro1090 <78980929+pedro1090@users.noreply.github.com>
2021-03-17 11:20:48 -0300

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,14 +1,14 @@
 const blocked_domains = [
 	"*://*.doubleclick.net/*",
-	"*//*partner.googleadservices.com/*",
-	"*//*.googlesyndication.com/*",
-	"*//*.google-analytics.com/*",
-	"*//*creative.ak.fbcdn.net/*",
-	"*//*.adbrite.com/*",
-	"*//*.exponential.com/*",
-	"*//*.quantserve.com/*",
-	"*//*.scorecardresearch.com/*",
-	"*//*.zedo.com/*",
+	"*://partner.googleadservices.com/*",
+	"*://*.googlesyndication.com/*",
+	"*://*.google-analytics.com/*",
+	"*://creative.ak.fbcdn.net/*",
+	"*://*.adbrite.com/*",
+	"*://*.exponential.com/*",
+	"*://*.quantserve.com/*",
+	"*://*.scorecardresearch.com/*",
+	"*://*.zedo.com/*",
 ]
 var enabled = true;
 //função para verificar os sites antes da request
