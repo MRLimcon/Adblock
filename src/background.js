bc5de091817777028929f87df3283cc0bdf0da19
pedro1090 <78980929+pedro1090@users.noreply.github.com>
2021-03-17 11:19:01 -0300

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,8 +1,20 @@
+const blocked_domains = [
+	"*://*.doubleclick.net/*",
+	"*//*partner.googleadservices.com/*",
+	"*//*.googlesyndication.com/*",
+	"*//*.google-analytics.com/*",
+	"*//*creative.ak.fbcdn.net/*",
+	"*//*.adbrite.com/*",
+	"*//*.exponential.com/*",
+	"*//*.quantserve.com/*",
+	"*//*.scorecardresearch.com/*",
+	"*//*.zedo.com/*",
+]
 var enabled = true;
 //função para verificar os sites antes da request
 chrome.webRequest.onBeforeRequest.addListener(
 	//se o site for igual a um dos sites do blocked_domains (está em blocked_domains.js), a request é cancelada
-	function(details) { return {cancel: true}; },
+	function(details) { return {cancel: true}},
 	{urls: blocked_domains},
 	["blocking"]
-);
+)
