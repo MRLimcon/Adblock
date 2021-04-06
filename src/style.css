32424dcc63fbaa9b5ee2c13f4aea4dc859176a7a
Pedro Barbosa <78980796+jakjax1902@users.noreply.github.com>
2021-04-06 00:03:31 -0300

    Arquivo css e png
    Arquivo css com os layouts para o popup.html e imagem png com a logo do Tatu Blocker

---
+++ src/style.css
@@ -0,0 +1,38 @@
+body {
+    background-color: rgb(3,37,62);
+    width: 5.65cm;
+    height: 9cm;
+}
+
+img{
+    height: 1.5cm;
+    width: auto;
+    opacity: 0.5;
+}
+.center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+h1 {
+    color: white;
+    font-size: auto;
+    text-align: center;
+    margin-left: auto;
+}
+
+h2{
+    color: white;
+    font-size: small;
+}
+
+p{
+    color: white;
+}
+
+.select{
+    color: rgb(0, 0, 0);
+}
+
+
